f978232748ac39eccf35fe04f3bc43f9b992beb3
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-10 23:23:30 +0300

    Added font family in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+
+import "@fontsource/open-sans";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/600.css";
 import './index.css'
+
 import { ThemeProvider } from 'styled-components'
 
 import theme from './theme.tsx';
