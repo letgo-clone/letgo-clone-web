8d537e29d8ce5e70a27fc2ea705b51b0f654f51c
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-11 14:45:13 +0300

    Edited sub footer in footer for mobile devices

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Container } from "@mui/material"
+import { Grid, Container, Typography } from "@mui/material"
 
 import { Facebook, Instagram, Twitter, YouTube } from '@mui/icons-material';
 
@@ -66,16 +66,22 @@ function Footer() {
             </Container>
             <Grid container sx={{ backgroundColor: '#ff3f55' }}>
                 <Container>
-                    <Grid container sx={{ marginTop: '25px', marginBottom: '15px', color: '#FFFFFF', fontSize:'12px' }}>
-                        <Grid md={6}>Resmi bağlantılar</Grid>
-                        <Grid md={6} sx={{ textAlign:'right' }}> <span style={{ fontWeight:'600', paddingRight:'12px' }}> Türkiye'de ikinci el eşya al ve sat</span> © 2006-2023 letgo</Grid>
+                    <Grid container sx={{ marginTop: '25px', marginBottom: '15px', color: '#FFFFFF', fontSize: '12px' }}>
+                        <Grid md={8} xs={12}>
+                            <Grid container>
+                                <Grid md={12} xs={12} sx={{ textAlign: { xs: 'center', md: 'left', marginBottom: '10px', fontWeight: 600 } }}>
+                                    Resmi bağlantılar
+                                </Grid>
+                                <Grid md={12} xs={12} sx={{ textAlign:'center', marginBottom: '30px', display: { md : 'inline-flex', xs: 'block' } }}>
+                                    <Typography sx={{ display:'inline-block', margin:'0px 4px 0px 0px', fontSize:'12px', lineHeight:'18px', fontWeight:300 }}>Şartlar ve Koşullar</Typography>
+                                    <Typography sx={{ display:'inline-block', margin:'0px 4px 0px 0px', fontSize:'12px', lineHeight:'18px', fontWeight:300 }}>-Reklam Politikası</Typography>
+                                    <Typography sx={{ display:'inline-block', margin:'0px 4px 0px 0px', fontSize:'12px', lineHeight:'18px', fontWeight:300 }}>-Resmi Kurumlar için Kılavuz</Typography>
+                                    <Typography sx={{ display:'inline-block', margin:'0px 4px 0px 0px', fontSize:'12px', lineHeight:'18px', fontWeight:300 }}>-Gizlilik Bildirimi</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid md={4} xs={12} sx={{ textAlign: { md: 'right', xs: 'center'}, fontSize:'12px', fontWeight:100 }}> <span style={{ fontWeight: '600', paddingRight: '12px' }}> Türkiye'de ikinci el eşya al ve sat</span> © 2006-2023 letgo</Grid>
                     </Grid>
-                    <ol id='footer-bottom-content'>
-                        <li>Şartlar ve Koşullar</li>
-                        <li>Reklam Politikası</li>
-                        <li style={{ maxWidth:'170px' }}>Resmi Kurumlar için Kılavuz</li>
-                        <li>Gizlilik Bildirimi</li>
-                    </ol>
                 </Container>
             </Grid>
         </footer>
