061f0709262b8b5854ccf47cd11df579ec06eeb0
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-12 18:51:58 +0300

    Added dummy data in home page for adCard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
 
+
+import { AdCard } from '../components/AdCard';
+
 function index() {
+  let dataAd: (object)[];
+
+  dataAd = [
+    {
+      id: 23,
+      price: 600,
+      photo: 'https://apollo-ireland.akamaized.net/v1/files/ynm4j6jj89vc2-OLXAUTOTR/image;s=780x0;q=60',
+      explain: 'Raf Kitaplık',
+      location: 'Sarıyer, İstanbul',
+      date: 'Bugün',
+    },
+    {
+      id: 24,
+      price: 600,
+      photo: 'https://apollo-ireland.akamaized.net/v1/files/j07mbdudw9ay-OLXAUTOTR/image;s=780x0;q=60',
+      explain: 'Raf Kitaplık',
+      location: 'Bağcılar, İstanbul',
+      date: 'Bugün',
+    },
+    {
+      id: 24,
+      price: 600,
+      photo: 'https://apollo-ireland.akamaized.net/v1/files/j07mbdudw9ay-OLXAUTOTR/image;s=780x0;q=60',
+      explain: 'Raf Kitaplık',
+      location: 'Bağcılar, İstanbul',
+      date: 'Bugün',
+    },
+    
+  ]
+  
   return (
-    <div></div>
+      <>
+        <AdCard data={dataAd} />
+      </>
   )
 }
 
