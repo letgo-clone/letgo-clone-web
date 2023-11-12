542676fb3abe876c79921dab549b380144f5976a
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-12 18:51:08 +0300

    Edited route index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 // Dashboard
-import Dashboard from "../pages/index";
+import Home from "../pages/index";
 
  const publicRoutes = [
-    { path: "/dashboard", component: <Dashboard /> },
-    { path: "/", exact: true, component: <Navigate to="/dashboard" /> },
+    { path: "/", component: <Home /> },
+    { path: "/", exact: true, component: <Navigate to="/" /> },
 ]; 
 
 export { publicRoutes };
