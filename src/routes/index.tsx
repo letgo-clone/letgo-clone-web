907f54e22ffad0fcf3d11290a66b3e81fca6c6dd
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-20 11:01:16 +0300

    Added advert detail in route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,13 @@ import { Navigate } from "react-router-dom";
 // Dashboard
 import Home from "../pages/Home/index";
 
+// Adverts
+import AdvertDetail from "../pages/Adverts";
+
  const publicRoutes = [
     { path: "/", component: <Home /> },
-    { path: "/", exact: true, component: <Navigate to="/" />}
+    { path: "/", exact: true, component: <Navigate to="/" />},
+    { path: "/item/:itemId", component: <AdvertDetail /> }
 ]; 
 
 export { publicRoutes };
