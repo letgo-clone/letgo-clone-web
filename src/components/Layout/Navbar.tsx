4af218c27b9ea8b766c660328d6bebf12a56cc4d
Fırat Yıldız <firatyildiznet@gmail.com>
2023-12-03 01:17:00 +0300

    Edited navbar after login

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -53,10 +53,28 @@ function Navbar() {
         setOpen(false);
     };
 
+    const [login, setLogin] = useState(false);
+
+    useEffect(() => {
+        if (localStorage.getItem('access_token')) {
+            setLogin(true)
+        }
+    }, [])
+
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const LoginOpen = Boolean(anchorEl);
+
+    const handleLoginClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleLoginClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <AppBar position="static" sx={{ bgcolor: 'hsla(0,0%,100%,.87)', boxShadow: 1 }}>
             <Container
-                maxWidth='lg'
+                maxWidth='xl'
                 sx={{
                     marginTop: '5px',
                     marginBottom: '5px',
@@ -146,8 +164,100 @@ function Navbar() {
                             </Paper>
                         </Grid>
                     </Grid>
-                    <Box sx={{ display: { md: 'flex', sm: 'flex', xs: 'none' }, marginLeft: '20px' }}>
-                        <Button onClick={handleOpen} sx={{ color: '#ff3f55' }}><p style={{ fontWeight: '600', borderBottom: '2px solid #ff3f55', textTransform: 'none' }}>Giriş</p></Button>
+                    <Box sx={{ display: { md: 'inline-flex', sm: 'inline-flex', xs: 'none' }, marginLeft: '20px' }}>
+                        {login ? (
+                            <Grid container spacing={3}>
+                                <Grid item xl={2} lg={2} md={2}>
+                                    <IconButton>
+                                        <SmsIcon />
+                                    </IconButton>
+                                </Grid>
+                                <Grid item xl={2} lg={2} md={2}>
+                                    <IconButton>
+                                        <NotificationsIcon />
+                                    </IconButton>
+                                </Grid>
+                                <Grid item xl={8} lg={2} md={2}>
+                                    <Tooltip title="Account settings">
+                                        <IconButton
+                                            onClick={handleLoginClick}
+                                            size="small"
+                                            sx={{ ml: 2 }}
+                                            aria-controls={LoginOpen ? 'account-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={LoginOpen ? 'true' : undefined}
+                                        >
+                                            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                                        </IconButton>
+                                    </Tooltip>
+                                </Grid>
+                                <Menu
+                                    anchorEl={anchorEl}
+                                    id="account-menu"
+                                    open={LoginOpen}
+                                    onClose={handleLoginClose}
+                                    onClick={handleLoginClose}
+                                    PaperProps={{
+                                        elevation: 0,
+                                        sx: {
+                                            overflow: 'visible',
+                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                            mt: 1.5,
+                                            '& .MuiAvatar-root': {
+                                                width: 32,
+                                                height: 32,
+                                                ml: -0.5,
+                                                mr: 1,
+                                            },
+                                            '&:before': {
+                                                content: '""',
+                                                display: 'block',
+                                                position: 'absolute',
+                                                top: 0,
+                                                right: 14,
+                                                width: 10,
+                                                height: 10,
+                                                bgcolor: 'background.paper',
+                                                transform: 'translateY(-50%) rotate(45deg)',
+                                                zIndex: 0,
+                                            },
+                                        },
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <MenuItem onClick={handleClose}>
+                                        <Avatar /> Profile
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <Avatar /> My account
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem onClick={handleClose}>
+                                        <ListItemIcon>
+                                            {/* <PersonAdd fontSize="small" /> */}
+                                        </ListItemIcon>
+                                        Add another account
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <ListItemIcon>
+                                          {/*   <Settings fontSize="small" /> */}
+                                        </ListItemIcon>
+                                        Settings
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <ListItemIcon>
+                                           {/*  <Logout fontSize="small" /> */}
+                                        </ListItemIcon>
+                                        Logout
+                                    </MenuItem>
+                                </Menu>
+                            </Grid>
+                        ) : (
+                            <>
+                                <Button onClick={handleOpen} sx={{ color: '#ff3f55' }}><p style={{ fontWeight: '600', borderBottom: '2px solid #ff3f55', textTransform: 'none' }}>Giriş</p></Button>
+                            </>
+                        )}
                         <Link to="/post">
                             <Button
                                 sx={
