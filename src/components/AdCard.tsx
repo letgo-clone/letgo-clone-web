147355328766754563e017c265d38228533df9d8
Fırat Yıldız <firatyildiznet@gmail.com>
2023-12-04 23:43:15 +0300

    Edited the adCard compmnt for can add favorite advert

--- src/components/AdCard.tsx
+++ src/components/AdCard.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Grid, Card, CardActions, CardContent, CardMedia, Typography, Chip, Button } from '@mui/material';
 
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import IconButton from '@mui/material/IconButton';
 import BoltIcon from '@mui/icons-material/Bolt';
 import CallIcon from '@mui/icons-material/Call';
 
-import Link from '@mui/material/Link';
+import { Link } from 'react-router-dom';
 
 import styles from "../assets/css/cards.module.css";
 
@@ -15,51 +16,117 @@ import otoplusBadge from '../assets/img/otoplus-badge.png'
 
 import slugify from 'react-slugify';
 
+import { Request } from '../helpers/Request';
+import Swal from 'sweetalert2';
+
 type AdvertProps = {
     data: string[];
     grid: number[];
 }
 
 export const AdCard = ({ data, grid }: AdvertProps) => {
+    const [cardData, setCardData] = useState(data);
+
+    const addFavorite = async (advertId: number, hasFavorite: any) => {
+
+        if(hasFavorite){
+            const formdata: FormData = new FormData();
+            formdata.append("op", 'remove');
+            formdata.append("path", 'has_advert_favorite');
+
+            const url = '/advert/favorite/' + advertId;
+            const data = await Request('PATCH', url, formdata);
+
+            if(data.error){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Hata',
+                    text: 'Bi Hata oluştu',
+                  })
+            }
+
+            setCardData(prevObjects => {
+                return prevObjects.map(obj => {
+                  if (obj.id === advertId) {
+                    return { ...obj, has_favorite: false }
+                  }
+                  return obj;
+                })
+            })  
+        }
+        else
+        {
+            const formdata: FormData = new FormData();
+            formdata.append("op", 'add');
+            formdata.append("path", 'has_advert_favorite');
+
+            const url = '/advert/favorite/' + advertId;
+            const data = await Request('PATCH', url, formdata);
+
+            if(data.error){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Hata',
+                    text: 'Bi Hata oluştu',
+                  })
+            }
+
+            setCardData(prevObjects => {
+                return prevObjects.map(obj => {
+                  if (obj.id === advertId) {
+                    return { ...obj, has_favorite: true }
+                  }
+                  return obj;
+                })
+            })  
+        }
+    }
 
     return (
         <Grid container spacing={2}>
-            {data.map((item, index) => (
+            {cardData.map((item, index) => (
                 <Grid item={true} lg={grid[0]} md={grid[1]} sm={grid[2]} xs={grid[3]} key={index}>
                     <Card className={styles.card}>
-                    <Link href={`/item/${slugify(item.title)}?id=${item.id}`} sx={{ textDecoration: 'none' }}>
+
                         <div className={styles.cardMediaDiv}>
-                            <CardMedia
-                                component="img"
-                                className={styles.cardMedia}
-                                image={item.images[0].url}
-                            />
-                            {(item.display_type == 'hot' && item.user_type !== 'OTOPLUS') && (
-                                <div className={styles.cardActionLeft} >
-                                    <Chip
-                                        avatar={<BoltIcon />}
-                                        label="Öne Çıkan"
-                                        variant="outlined"
-                                        size="small"
-                                        className={styles.actionFeatured}
-                                    />
-                                </div>
-                            )}
-                            {(item.user_type == 'OTOPLUS') && (
-                                <div className={styles.cardActionLeft} >
-                                    <Chip
-                                        label={<img src={otoplusBadge} />}
-                                        variant="outlined"
-                                        size="small"
-                                        className={styles.actionOtoplusFeatured}
-                                    />
-                                </div>
-                            )}
-                            <div className={styles.cardActionRight} >
+                            <div className={styles.cardActionRight} onClick={() => addFavorite(item.id, item.has_favorite)}>
                                 <IconButton aria-label="add to favorites" sx={{ padding: 0 }}>
-                                    <FavoriteIcon />
+                                    {item.has_favorite ? (
+                                        <FavoriteBorderIcon sx={{ color:'red' }} />
+                                    ): (
+                                        <FavoriteIcon sx={{ color:'red' }} />
+                                    )}
+                                   
                                 </IconButton>
                             </div>
+                            <Link to={`/item/${slugify(item.title)}?id=${item.id}`} style={{ textDecoration: 'none' }}>
+                                <CardMedia
+                                    component="img"
+                                    className={styles.cardMedia}
+                                    image={item.images[0].url}
+                                />
+                                {(item.display_type == 'hot' && item.user_type !== 'OTOPLUS') && (
+                                    <div className={styles.cardActionLeft} >
+                                        <Chip
+                                            avatar={<BoltIcon />}
+                                            label="Öne Çıkan"
+                                            variant="outlined"
+                                            size="small"
+                                            className={styles.actionFeatured}
+                                        />
+                                    </div>
+                                )}
+                                {(item.user_type == 'OTOPLUS') && (
+                                    <div className={styles.cardActionLeft} >
+                                        <Chip
+                                            label={<img src={otoplusBadge} />}
+                                            variant="outlined"
+                                            size="small"
+                                            className={styles.actionOtoplusFeatured}
+                                        />
+                                    </div>
+                                )}
+                            </Link>
                         </div>
                         {item.user_type != 'OTOPLUS' ? (
                             item.display_type == 'hot' ? (
@@ -75,14 +142,14 @@ export const AdCard = ({ data, grid }: AdvertProps) => {
                                     <CardActions>
                                         <Grid container className={styles.cardFooter}>
                                             <Grid item={true} md={6} sm={6} xs={6}>
-                                                 <Typography variant="body2" color="text.secondary" sx={{ fontSize: '10px' }}>
+                                                <Typography variant="body2" color="text.secondary" sx={{ fontSize: '10px' }}>
                                                     {item.county},{item.city}
-                                                </Typography> 
+                                                </Typography>
                                             </Grid>
                                             <Grid item={true} md={6} sm={6} xs={6} sx={{ float: 'right' }}>
-                                                 <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'right', fontSize: '10px' }}>
+                                                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'right', fontSize: '10px' }}>
                                                     {item.date}
-                                                </Typography> 
+                                                </Typography>
                                             </Grid>
                                         </Grid>
                                     </CardActions>
@@ -103,14 +170,14 @@ export const AdCard = ({ data, grid }: AdvertProps) => {
                                     <CardActions>
                                         <Grid container className={styles.cardFooter}>
                                             <Grid item={true} md={6} sm={6} xs={6}>
-                                                 <Typography variant="body2" color="text.secondary" sx={{ fontSize: '10px' }}>
+                                                <Typography variant="body2" color="text.secondary" sx={{ fontSize: '10px' }}>
                                                     {item.county},{item.city}
                                                 </Typography>
                                             </Grid>
                                             <Grid item={true} md={6} sm={6} xs={6} sx={{ float: 'right' }}>
                                                 <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'right', fontSize: '10px' }}>
                                                     {item.date}
-                                                </Typography> 
+                                                </Typography>
                                             </Grid>
                                         </Grid>
                                     </CardActions>
@@ -129,16 +196,16 @@ export const AdCard = ({ data, grid }: AdvertProps) => {
                                         <Grid container className={styles.cardFooter}>
 
                                             <Grid item={true} md={6} sm={6} xs={6}>
-                                            {item.city && (
-                                                 <Typography variant="body2" color="text.secondary" sx={{ fontSize: '10px' }}>
+                                                {item.city && (
+                                                    <Typography variant="body2" color="text.secondary" sx={{ fontSize: '10px' }}>
                                                         {item?.county}, {item?.city}
-                                                </Typography>
-                                            )}
+                                                    </Typography>
+                                                )}
                                             </Grid>
                                             <Grid item={true} md={6} sm={6} xs={6} sx={{ float: 'right' }}>
-                                                 <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'right', fontSize: '10px' }}>
+                                                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'right', fontSize: '10px' }}>
                                                     {item.date}
-                                                </Typography> 
+                                                </Typography>
                                             </Grid>
                                         </Grid>
                                     </CardActions>
@@ -155,8 +222,8 @@ export const AdCard = ({ data, grid }: AdvertProps) => {
                                     </Typography>
                                 </CardContent>
                                 <CardActions sx={{ display: 'grid' }}>
-                                    <Button 
-                                        variant="outlined" 
+                                    <Button
+                                        variant="outlined"
                                         startIcon={<CallIcon />}
                                         sx={{
                                             backgroundColor: '#ff3f55',
@@ -171,7 +238,7 @@ export const AdCard = ({ data, grid }: AdvertProps) => {
                                 </CardActions>
                             </div>
                         )}
-                        </Link>
+
                     </Card>
                 </Grid>
             ))}
