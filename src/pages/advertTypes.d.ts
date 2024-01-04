a7a29c6b986a792d9b789f59d40a71024f251435
Fırat Yıldız <firatyildiznet@gmail.com>
2024-01-04 14:14:43 +0300

    Changed position the adcardTypes

--- src/pages/advertTypes.d.ts
+++ src/pages/advertTypes.d.ts
@@ -1,3 +1,31 @@
+export type AdvertProps = {
+    data: CardTypes[] | CardTypes;
+    grid: number[];
+}
+
+export interface FavoriteProps {
+    error?: {
+        message?: string
+    },
+    success?: boolean
+}
+
+export interface CardTypes {
+    city?: string,
+    county?: string,
+    date?: string,
+    description?: string,
+    display_name?: string,
+    display_type?: string
+    has_favorite?: boolean,
+    how_status?:string,
+    id?:string,
+    photo?: string,
+    price?:string,
+    title?:string,
+    user_type?:string
+}
+
 export interface DetailCardTypes {
     city?: string,
     county?: string,
@@ -8,21 +36,20 @@ export interface DetailCardTypes {
     fullname?: string
     how_status?: string,
     id?: string
-    price?:string
-    title?:string
-    user_image?:{ 
-        url: string, 
+    price?: string
+    title?: string
+    user_image?: {
+        url: string,
         path: string
     },
-    user_type?:string,
-    userid?:number,
+    user_type?: string,
+    userid?: number,
     photo?: {
         height?: string,
-        images_id?:string,
-        is_cover_image?:boolean,
-        path?:string,
-        url?:string
-        width?:string
+        images_id?: string,
+        is_cover_image?: boolean,
+        path?: string,
+        url?: string
+        width?: string
     }[]
 }
-
