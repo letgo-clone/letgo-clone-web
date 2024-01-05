0b650e9e5482b3932883da681bb1c20f74a54213
Fırat Yıldız <firatyildiznet@gmail.com>
2024-01-05 18:01:10 +0300

    Created the PostAdvertTypes for form type

--- src/pages/formTypes.d.ts
+++ src/pages/formTypes.d.ts
@@ -4,3 +4,14 @@ export interface ProfileInfoTypes {
     phoneNumber?: string,
     email?: string
 }
+
+export interface PostAdvertTypes {
+    title?: string,
+    description?: string,
+    price?: string,
+    city_id?: string,
+    county_id?: string,
+    how_status?: string,
+    fullname?: string,
+    photo?: object
+}
