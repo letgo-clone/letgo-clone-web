90cd8dc70fcaa4af1407698ec0e5e70611d7da55
Fırat Yıldız <firatyildiznet@gmail.com>
2024-01-07 14:09:15 +0300

    Added get form in search component

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Material UI elements
 import { 
     Grid, 
-    IconButton, 
     MenuItem, 
     FormControl, 
     Paper, 
@@ -11,6 +10,8 @@ import {
     ListItem,
     ListItemIcon,
     ListItemText,
+    Container,
+    Button,
     InputBase } from "@mui/material"
 
 // Material UI Icons
@@ -22,67 +23,118 @@ import {
 // styles
 import { searchStyles } from '../../styles';
 
+// Helper
+import { RequestPublic } from '../../helpers/Request';
+
+// other
+import { useFormik } from 'formik';
+import slugify from 'react-slugify';
+
 // İnterface
-import { searchProps } from './commonTypes';
+import { 
+    searchProps
+    ,searchFormTypes 
+    } from './commonTypes';
+
+import { CountiesProps } from '../../pages/advertTypes';
 
 const Search: React.FC<searchProps> = ({ dimension }) => {
-   
+    const cityId = '34';
+    const [counties, setCounties] = useState<CountiesProps[]>([]);
+
+    useEffect(() => {
+        const getCounties = async() => {
+            const url = "/advert/location/" + 34;
+            const result = await RequestPublic({
+                method: 'GET',
+                url: url
+            })
+
+            setCounties(result);
+        }
+        getCounties();
+      
+    },[])
+
+   const initialValues: searchFormTypes = {
+        cityId: cityId,
+        location: '',
+        search: '',
+    }
+
+    const formik = useFormik({
+        enableReinitialize: true,
+        initialValues,
+        onSubmit: async (values) => {
+            const {cityId, location, search} = values;
+
+            const searchFilter = slugify(search);
+           
+        }
+    })
   return (
-    <Grid container>
-        {/* Location select input */}
-        <Grid item xl={4} md={4} xs={4}>
-            <FormControl size="small" fullWidth>
-                <Select
-                    id="location"
-                    name="location"
-                    value="34"
-                    sx={searchStyles.selectLocation}
-                >
-                    <MenuItem value="34">
-                        <ListItem sx={searchStyles.selectLocationListItem}>
-                            <ListItemIcon sx={searchStyles.selectLocationListItemFirstIcon}>
-                                <LocationOn />
-                            </ListItemIcon>
-                            <ListItemText primary="İstanbul, Türkiye" />
-                        </ListItem>
-                    </MenuItem>
-                    <MenuItem value="06">
-                        <ListItem sx={searchStyles.selectLocationListItem}>
-                            <ListItemIcon>
-                                <LocationOn />
-                            </ListItemIcon>
-                            <ListItemText primary="Ankara" />
-                        </ListItem>
-                    </MenuItem>
-                    <MenuItem value="35">
-                        <ListItem sx={searchStyles.selectLocationListItem}>
-                            <ListItemIcon>
-                                <LocationOn />
-                            </ListItemIcon>
-                            <ListItemText primary="İzmir" />
-                        </ListItem>
-                    </MenuItem>
-                </Select>
-            </FormControl>
-        </Grid>
-        {/* Search input */}
-        <Grid item xl={8} md={8} xs={12} sx={searchStyles.inputSearchGrid}>
-            <Paper component="form" sx={searchStyles.inputSearchPaper}>
-                <InputBase
-                    sx={searchStyles.inputSearchAreaInputBase}
-                    placeholder="Araba, telefon, bisiklet ve daha fazlası"
-                    inputProps={{ 'aria-label': 'search google maps' }}
-                />
-                <IconButton
-                    href='/item/search'
-                    color="primary"
-                    sx={searchStyles.searchInputIconButton}
-                    aria-label="directions">
-                    <SearchOutlined />
-                </IconButton>
-            </Paper>
-        </Grid>
-    </Grid>
+    <Container>
+        <form
+            onSubmit={formik.handleSubmit}
+        >
+            <Grid container>
+                    {/* Location select input */}
+                    <Grid item xl={4} md={4} xs={4}>
+                        <FormControl size="small" fullWidth>
+                            <Select
+                                id="location"
+                                name="location"
+                                sx={searchStyles.selectLocation}
+                                value={formik.values.location}
+                                onChange={formik.handleChange}
+                                error={Boolean(formik.values.location == '' && formik.touched.location)}
+                            >
+                                <MenuItem value="34">
+                                    <ListItem sx={searchStyles.selectLocationListItem}>
+                                        <ListItemIcon sx={searchStyles.selectLocationListItemFirstIcon}>
+                                            <LocationOn />
+                                        </ListItemIcon>
+                                        <ListItemText primary="İstanbul, Türkiye" />
+                                    </ListItem>
+                                </MenuItem>
+                                {counties.length > 0 && counties.map((item, key) => (
+                                    <MenuItem value={item.id} key={key}>
+                                        <ListItem sx={searchStyles.selectLocationListItem}>
+                                            <ListItemIcon>
+                                                <LocationOn />
+                                            </ListItemIcon>
+                                            <ListItemText primary={item.county} />
+                                        </ListItem>
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    {/* Search input */}
+                    <Grid item xl={8} md={8} xs={12} sx={searchStyles.inputSearchGrid}>
+                        <Paper sx={searchStyles.inputSearchPaper}>
+                            <InputBase
+                                name="search"
+                                placeholder="Araba, telefon, bisiklet ve daha fazlası"
+                                inputProps={{ 'aria-label': 'search google maps' }}
+                                sx={searchStyles.inputSearchAreaInputBase}
+                                value={formik.values.search}
+                                onChange={formik.handleChange}
+                                error={Boolean(formik.values.search == '' && formik.touched.search)}
+                            />
+                            <Button
+                                type="submit"
+                                color="primary"
+                                sx={searchStyles.searchInputIconButton}
+                                aria-label="directions">
+                                <SearchOutlined />
+                            </Button>
+                        </Paper>
+                    </Grid>
+                   
+            </Grid>
+        </form>
+    </Container>
   )
 }
 
--- src/components/common/commonTypes.d.ts
+++ src/components/common/commonTypes.d.ts
@@ -1,3 +1,9 @@
 export type searchProps = {
     dimension: string
 }
+
+export interface searchFormTypes {
+    cityId?: string,
+    location?: string,
+    search?: string
+}
